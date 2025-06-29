2ce4a17d7339ffa72f61e8c2e3b9d7e8313ed813
Mahmoud Ayman <mahmoud_aymann@outlook.com>
2025-06-30 01:02:32 +0300

    add glide lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.exoplayer.dash)
     implementation(libs.androidx.media3.ui)
+    implementation(libs.github.glide)
 }
